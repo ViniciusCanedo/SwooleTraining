c420ed87075db48c93d1da9f3a9d2b8fb1e5cbe1
ViniciusCanedo <warwick0201@gmail.com>
2023-12-17 19:29:51 -0300

    pattern: Configured folder and file structure

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-phpinfo();
-?>
+
+use App\Server\Database;
+
+require __DIR__.'/vendor/autoload.php';
+
+$obDatabase = new Database('swoole');
+echo '<pre>';
+print_r($obDatabase);
+echo '</pre>';
+exit;
