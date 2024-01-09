64b200858299536e91eb7b0c76236c4112627133
ViniciusCanedo <warwick0201@gmail.com>
2024-01-08 23:02:39 -0300

    raw: Added DAO queries in project

--- app/Server/Database.php
+++ app/Server/Database.php
@@ -51,4 +51,40 @@ class Database
 
         return $this->connection->lastInsertId();
     }
+
+    public function select($where = null, $columns = [], $join = null, $groupOrder = null, $limit = null)
+    {
+        $where = strlen($where) ? 'WHERE ' . $where : '';
+        $columns = empty($columns) ? '*' : implode(", ", $columns); 
+        $join = strlen($join) ? $join : '';
+        $groupOrder = strlen($groupOrder) ? $groupOrder : '';
+        $limit = strlen($limit) ? 'LIMIT ' . $limit : '';
+
+        $query = 'SELECT ' .$columns. ' FROM ' . $this->table . ' ' .$join. ' ' .$where. ' ' .$groupOrder. ' ' .$limit;
+
+        return $this->execute($query);
+    }
+
+    public function update($where, $set = null) 
+    {
+        $where = strlen($where) ? 'WHERE ' .$where : '';
+        $set = strlen($set) ? 'SET ' . $set : ''; 
+
+        $query = 'UPDATE ' .$this->table. ' ' .$set. ' ' .$where;
+        
+        $this->execute($query);
+
+        return true;
+    }
+
+    public function delete($where)
+    {
+        $where = strlen($where) ? 'WHERE ' .$where : '';
+
+        $query = 'DELETE FROM ' .$this->table. ' ' .$where;
+
+        $this->execute($query);
+
+        return true;
+    }
 }
